e1d9ec336b9d36e93e7cee5aa392731cf2109e57
Mia Tang <58094634+miatang13@users.noreply.github.com>
2021-01-13 00:10:40 -0800

    Create constants.js

---
+++ collider-ball/constants.js
@@ -0,0 +1,60 @@
+/******************* FILE OVERVIEW ***************/
+/* This file contains the constant values used
+ * in sketch.js
+ */
+
+/******************* CREDITS *********************/
+/* Author: Mia Tang (www.mia-tang.com, miatang13@gmail.com)
+ * Thanks to Friend Fan Bu's help on physics
+ */
+
+/* to turn off p5.js function / variables undefined warnings */
+/*eslint no-undef: 0*/
+
+
+/******************* COLORS *********************/
+const DARK_PURPLE = "#792BB2";
+const BLUE = "#2E42CB";
+const PINK = "#F75781";
+const PURPLE = "#E365E4";
+const ORANGE = "#FA5348";
+const DARK_BEIGE = "#DFC7C1";
+const BEIGE = "#F4DCD6";
+const LIGHT_BLUE = "#B2D9EA";
+const MUTED_BLUE = "#84B4C8";
+const TURQ = "#619196";
+const PASTEL_GREEN = "#D7E7A9";
+
+const LITTLE_CIRCLE_COLOR = [DARK_PURPLE];
+const PARTICLE_COLOR = [PASTEL_GREEN, BEIGE, LIGHT_BLUE];
+const LITTLE_CIRCLE_OUTLINE = "grey";
+const PARTICLE_OUTLINE = BEIGE;
+
+/******************* LITTLE CIRCLE***************/
+const LITTLE_CIRCLE_RADIUS = 6;
+const LITTLE_CIRCLE_WIDTH = 0;
+const LITTLE_CIRCLE_NUM = 30;
+
+/******************* PARTICLE *******************/
+const PARTICLE_NUM = 200;
+const PARTICLE_VX_MIN = 50;
+const PARTICLE_VX_MAX = 300;
+const PARTICLE_VY_MIN = 50;
+const PARTICLE_VY_MAX = 300;
+const PARTICLE_WIDTH = 0;
+const PARTICLE_CX_MIN = -100;
+const PARTICLE_CX_MAX = 100;
+const PARTICLE_CY_MIN = -100;
+const PARTICLE_CY_MAX = 100;
+const PARTICLE_RADIUS_MIN = 2;
+const PARTICLE_RADIUS_MAX = 7;
+
+/******************* PLANET *********************/
+const PLANET_RADIUS_Y = 120;
+const PLANET_RADIUS_X = 120;
+const PLANET_VX = 15;
+const PLANET_VY = 18;
+const PLANET_DECELERATION_SPEED = 18;
+
+/******************* TRACE *********************/
+const TC_MAX = 1;
